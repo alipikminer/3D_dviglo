93c86bfd5d92632635d91ef244c06b61f81598a1
alipikminer <alipikminer@yandex.ru>
2017-05-01 15:07:30 +0300

    zsd
    sd

--- 3d двигло.pas
+++ 3d двигло.pas
@@ -2,7 +2,8 @@ uses GraphAbc;
 
 var cubex1,cubex2,cubey1,cubey2,cubex3,cubex4,cubey3,cubey4: integer;
 var cubux1,cubux2,cubuy1,cubuy2:real;
-var dalnost:integer;
+var dalnost,sel:integer;
+var i,ii: integer;
 begin
 
 cubux1:=50;
@@ -10,7 +11,8 @@ cubux2:=150;
 cubuy1:=50;
 cubuy2:=150;
 repeat
-readln(dalnost);
+readln(sel);
+if sel=1 then readln(dalnost);
 cubex1:=round(int(cubux1/10));
 cubex2:=round(int(cubux2/10));
 cubey1:=round(int(cubuy1/10));
@@ -19,12 +21,12 @@ cubex3:=round(int((cubux1/10)+dalnost));
 cubex4:=round(int((cubux2/10)+dalnost));
 cubey3:=round(int((cubuy1/10)+dalnost));
 cubey4:=round(int((cubuy2/10)+dalnost));
-{
-cubex1:=cubux1;
-cubex2:=cubux2;
-cubey1:=cubuy1;
-cubey2:=cubuy2;
-}
+
+for i:= 1 to 100 do
+begin
+line(i,0,i,100,clWhite);
+end;
+
 line(cubex1,cubey1,cubex2,cubey1,clBlack);
 line(cubex2,cubey1,cubex2,cubey2,clBlack);
 line(cubex1,cubey2,cubex2,cubey2,clBlack);
@@ -39,6 +41,16 @@ line(cubex3,cubey3,cubex1,cubey1,clBlack);
 line(cubex3,cubey4,cubex1,cubey2,clBlack);
 line(cubex4,cubey3,cubex2,cubey1,clBlack);
 line(cubex4,cubey4,cubex2,cubey2,clBlack);
+textOut(512,10,'Current selected :');
+case sel of
+1:textOut(512,30,'Z scale' );
+2:textOut(512,30,'Scale');
+3:textOut(512,30,'Position');
+
+end;
+textOut(512,50,'Z scale: '+dalnost );
+textOut(512,70,'Scale:  ');
+textOut(512,90,'Position:  ');
 until 2=3;
 
 end.
